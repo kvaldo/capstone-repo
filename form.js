072ae71461fe5cb1f182663c4395cf72b93ddbd8
kvaldo <123497600+kvaldo@users.noreply.github.com>
2025-02-11 10:07:37 -0700

    Updated Front-End - 2/11/25

---
+++ form.js
@@ -0,0 +1,75 @@
+console.log("form.js is running...");
+
+const main = document.getElementById('scamform');
+const firstname = document.getElementById('firstname');
+const lastname = document.getElementById('lastname');
+const phonenumber = document.getElementById('phonenumber');
+const address = document.getElementById('address');
+const dob = document.getElementById('dob');
+
+const fnameError = document.getElementById('fnameError');
+const lnameError = document.getElementById('lnameError');
+const phoneError = document.getElementById('phoneError');
+const addrError = document.getElementById('addrError');
+const dobError = document.getElementById('dobError');
+
+console.log(main, firstname, lastname, dob)
+
+main.addEventListener("submit", (evt) => {
+    console.log('checking validity')
+
+    let isValid = false;
+
+    let namePattern = /\S{2,}/;
+    let fnameIsValid = namePattern.test(firstname.value);
+    let lnameIsValid = namePattern.test(lastname.value);
+
+    let phonePattern = /[0-9 ()-]{10,20}/;
+    let phoneIsValid = phonePattern.test(phonenumber.value);
+
+    let addressPattern = /\S{1,} \S{1,}/;
+    let addressIsValid = addressPattern.test(address.value);
+
+    let dobPattern = /\S{2,}/;
+    let dobIsValid = dobPattern.test(dob.value);
+
+    isValid = fnameIsValid && lnameIsValid && phoneIsValid && addressIsValid && dobIsValid
+
+    if (isValid) {
+
+    } else {
+        firstname.classList.remove("invalid");
+        lastname.classList.remove("invalid");
+        address.classList.remove("invalid");
+        phonenumber.classList.remove("invalid");
+        dob.classList.remove("invalid")
+        fnameError.innerText = "";
+        lnameError.innerText = "";
+        address.innerText = "";
+        phoneError.inner = "";
+        dobError.innerText = "";
+
+        if (!fnameIsValid){
+            firstname.classList.add("invalid");
+            fnameError.innerText = "Required at least 2 characters"
+        }
+        if (!lnameIsValid){
+            lastname.classList.add("invalid");
+            lnameError.innerText = "Required at least 2 characters"
+        }
+        if (!phoneIsValid){
+            phonenumber.classList.add("invalid");
+            phoneError.innerText = "Enter Valid Phone Number (ex: (XXX)XXX-XXXX)"
+        }
+        if (!addressIsValid){
+            address.classList.add("invalid");
+            addrError.innerText = 'Enter Valid Address line';
+        }
+        if (!dobIsValid){
+            dob.classList.add("invalid")
+            dobError.innerText = "Enter Valid Date"
+        }
+
+        evt.preventDefault()
+    };
+});
--- front-end.js
+++ front-end.js
@@ -22,17 +22,71 @@ app.get("/form", (req, res) => {
         lname: '',
         addr: '',
         phone: '',
-        email: '',
+//        email: '',
         dob: '',
-        schemail: '',
-        ccn: '',
-        bank: '',
-        ssn: '',
-        empass: '',
-        schpass: ''
+//        schemail: '',
+//        ccn: '',
+//        bank: '',
+//        ssn: '',
+//        empass: '',
+//        schpass: ''
     };
     res.render("form", model)
 })
+
+app.post("/form", async (req, res) => {
+    console.log("Form submitted")
+    console.log("Form Data: ", req.body)
+
+    let dataIsValid = false;
+    let userData = req.body;
+
+    let namePattern = /\S{2,}/;
+    let fnameIsValid = namePattern.test(userData.firstname);
+    let lnameIsValid = namePattern.test(userData.lastname);
+
+    let phonePattern = /[0-9 ()-]{10,20}/;
+    let phoneIsValid = phonePattern.test(userData.phonenumber);
+
+    let addressPattern = /\S{1,} \S{1,}/;
+    let addressIsValid = addressPattern.test(userData.address);
+
+    let dobPattern = /\S{2,}/;
+    let dobIsValid = dobPattern.test(userData.dob);
+
+    dataIsValid = fnameIsValid && lnameIsValid && phoneIsValid && addressIsValid && dobIsValid
+
+    if (dataIsValid) {
+        let formData = {
+            firstname: userData.firstname,
+            lastname: userData.lastname,
+            phonenumber: userData.phonenumber,
+            address: userData.address,
+            dob: userData.dob
+        };
+
+        let username = `${userData.firstname}${userData.lastname}`;
+
+        console.log(username, formData);
+
+        let url = "http://localhost:3000/createuser";
+
+        let options = {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        }
+
+        let p1 = await fetch(url, options);
+
+        res.redirect("/");
+    }else{
+        let model = req.body;
+        res.render("form", model)
+    }
+});
 
 app.listen(port, '10.0.103.20', () => {
     console.log(`Server running at http://10.0.103.20:${port}/`)
